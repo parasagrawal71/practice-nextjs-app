a924610b98a641a571a76a589eb7d3ec26b82c53
Paras Agrawal <parasagrawal71@gmail.com>
2025-04-23 21:45:55 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Greet from "./components/greet";
 import Counter from "./components/counter";
 
+// Reference Video: https://youtu.be/_EgI9WH8q1A?si=AM_nBUzPffK0tVq2
 export default function Home() {
   return (
     <div className={styles.page}>
